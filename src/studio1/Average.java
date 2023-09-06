05fa263c57889d7ae49c2fe81387f54f293fb1c1
WilliamLiu <WilliamLiu@localhost>
2023-09-06 17:16:27 -0500

    Studio 1 Groupwork

---
+++ src/studio1/Average.java
@@ -0,0 +1,18 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class Average {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("The first of two integers to be averaged?");
+		double n1 = in.nextDouble ();
+		System.out.println("The second of two integers to be averaged?");
+		double n2 = in.nextDouble ();
+		System.out.println ((n1 + n2) / 2);
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -15,7 +15,16 @@ public class Ordered {
 		int y = in.nextInt();
 		System.out.println("Value for z?");
 		int z = in.nextInt();
+boolean a1 = x < y;
+boolean a2 = y < z;
+boolean b1 = x > y;
+boolean b2 = y > z;
+boolean z1 = a1 && a2;
+boolean z2 = b1 && b2;
+boolean isOrdered = z1 || z2;
 
+	System.out.println(isOrdered);	
+	
 	}
 
 }
